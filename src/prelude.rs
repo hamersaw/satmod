c8e6edd5119140487aca82b6537fb744c2038f14
Daniel Rammer <hamersaw@protonmail.com>
2020-06-18 09:06:09 -0600

    added 'fix' function to prelude crate.

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
 pub use crate::coordinate::{Geocode, get_window_bounds};
-pub use crate::{copy_raster, coverage, /*fill,*/ init_dataset};
+pub use crate::{copy_raster, coverage, fill, init_dataset};
 pub use crate::serialize::{read, write};
 pub use crate::transform::{split};
